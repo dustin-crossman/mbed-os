dustin-crossman/mbed-os

commit 4f3f0cc9cc493a7ba57bd7dce9c17fff4a29ead5

Make Realtek link
There was some interesting stuff here.
 * ARMC6 treats assembly the same as GNU, but has a different startup
   function, so we branch the asm there
 * Incorrect discriminant, probably copied from other ports

targets/TARGET_Realtek/mbed_rtx.h
@@ -20,7 +20,7 @@
 
 #include "rtl8195a.h"
 
-#if defined(__CC_ARM)
+#if defined(__CC_ARM) || (defined(__ARMCC_VERSION) || (__ARMCC_VERSION >= 6010050))
     extern uint32_t             Image$$ARM_LIB_STACK$$ZI$$Base[];
     extern uint32_t             Image$$ARM_LIB_STACK$$ZI$$Length[];
     #define ISR_STACK_START     (unsigned char *)(Image$$ARM_LIB_STACK$$ZI$$Base)

targets/TARGET_Realtek/TARGET_AMEBA/TARGET_RTL8195A/device/rtl8195a_init.c
@@ -270,7 +270,11 @@ void PLAT_Main(void)
 #else
     __asm ("ldr  r0, =SystemInit   \n"
            "blx  r0                \n"
+#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
+           "ldr  r0, =__main \n"
+#else
            "ldr  r0, =_start       \n"
+#endif
            "bx   r0                \n"
     );
 #endif

commit 00b663b9c6811b626dbb4bea029cc18537f312ca

rtl8195am - fix ARMC6 guards
Fix ARMC6 guard typo introduced in commit 4f3f0cc9cc49 (Make Realtek link)

While at it, remove redundant ISR_STACK_SIZE assignment for ARMCC.

Signed-off-by: Tony Wu <tonywu@realtek.com>

targets/TARGET_Realtek/mbed_rtx.h
@@ -20,7 +20,7 @@
 
 #include "rtl8195a.h"
 
-#if defined(__CC_ARM) || (defined(__ARMCC_VERSION) || (__ARMCC_VERSION >= 6010050))
+#if defined(__CC_ARM) || (defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050))
     extern uint32_t             Image$$ARM_LIB_STACK$$ZI$$Base[];
     extern uint32_t             Image$$ARM_LIB_STACK$$ZI$$Length[];
     #define ISR_STACK_START     (unsigned char *)(Image$$ARM_LIB_STACK$$ZI$$Base)
@@ -33,8 +33,7 @@
     #define INITIAL_SP          (__StackTop)
 #endif
 
-
-#if defined(__CC_ARM) || defined(__GNUC__)
+#if defined(__GNUC__)
 #ifndef ISR_STACK_SIZE
 #define ISR_STACK_SIZE          (0x1000)
 #endif