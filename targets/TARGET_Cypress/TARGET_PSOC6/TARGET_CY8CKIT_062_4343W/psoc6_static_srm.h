e29db77091065c3fafa982ace78f751cfca23f3a
Cypress GitLab CI <>
2019-01-21 19:32:48 -0600

    cy-mbed-os-0.1.0.4769
    http://git-ore.aus.cypress.com/repo/psoc6sw/commit/f41795eeb97ee15e900687b1c2aaf40ae51c0fbe
    Andriy Mashchak <amak@cypress.com>
    updated SRM macro names to be CYCFG_ - based, updated comments (proof read pending)

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CKIT_062_4343W/psoc6_static_srm.h
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CKIT_062_4343W/psoc6_static_srm.h
@@ -17,19 +17,17 @@
  */
 
 /*
- * This file defines hardware resources statically allocated to M0 core
- * when static resource managemnt is used.
+ * This file defines hardware resources reserved by device generated code. These
+ * resources are accessed directly by PDL.
  *
  * There are 4 classes of resources that must be declared here:
- *  - M0_ASSIGNED_PORTS macro defines which ports and pins are reserved
- *    for M0 core use.
+ *  - CYCFG_ASSIGNED_PORTS macro defines which ports and pins are reserved.
  *    You define these as a colon separated list of ports and pins reserved
  *    using macro SRM_PORT(port_num, pins), one time for each reserved port.
  *    SRM_PORT macro arguments are port number, in the range 0 .. 14 and
  *    pins is a hex value with a bit set for each reserved pin on a port.
  *
- *  - M0_ASSIGNED_DIVIDERS macro defines which clock dividers are reserved
- *    for M0 core use.
+ *  - CYCFG_ASSIGNED_DIVIDERS macro defines which clock dividers are reserved.
  *    You define these as a colon separated list of dividers reserved
  *    using macro SRM_DIVIDER(type, reservations), one time for each required
  *    devider type.
@@ -37,31 +35,25 @@
  *    values and reservations is a hex mask value with a bit set for each
  *    reserved divider of a given type.
  *
- *  - M0_ASSIGNED_SCBS macro defines which SCB blocks are reserved
- *    for M0 core use.
+ *  - CYCFG_ASSIGNED_SCBS macro defines which SCB blocks are reserved.
  *    You define these as a colon separated list of SCBs reserved using
  *    macro SRM_SCB(n), which argument is SCB number in a range 0 .. 7.
  *
- *  - M0_ASSIGNED_TCPWM macro defines which TCPWM blocks are reserved
- *    for M0 core use.
+ *  - CYCFG_ASSIGNED_TCPWM macro defines which TCPWM blocks are reserved.
  *    You define these as a colon separated list of TCPWMs reserved using
  *    macro SRM_TCPWM(n), which argument is TCPWM number in a range 0 .. 31.
  *
- * If a particular resource class is not used at all by M0 core you can
- * skip defining relevant M0_ASSIGNED_* macro or define it as an empty one.
- *
  * Examples:
- *    #define M0_ASSIGNED_PORTS		SRM_PORT(0, 0x30), SRM_PORT(5, 0x03)
+ *    #define CYCFG_ASSIGNED_PORTS		SRM_PORT(0, 0x30), SRM_PORT(5, 0x03)
  *
- *    #define M0_ASSIGNED_DIVIDERS	SRM_DIVIDER(CY_SYSCLK_DIV_8_BIT, 0x01)
+ *    #define CYCFG_ASSIGNED_DIVIDERS	SRM_DIVIDER(CY_SYSCLK_DIV_8_BIT, 0x01)
  *
- *	  #define M0_ASSIGNED_SCBS		SRM_SCB(2)
+ *	  #define CYCFG_ASSIGNED_SCBS		SRM_SCB(2)
  *
- *    #define M0_ASSIGNED_TCPWMS
+ *    #define CYCFG_ASSIGNED_TCPWMS
  *
  */
 
-// Reservations below apply to default M0 hex image.
 
 /* P0_0 and P0_1 reserved for WCO, 
 *  P1_0 reserved for CSD TX,
