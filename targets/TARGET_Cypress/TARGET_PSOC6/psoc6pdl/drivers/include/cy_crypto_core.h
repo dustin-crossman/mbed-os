d8380796de484222808f28c079dea2226cbd90ff
Volodymyr Medvid <volodymyr.medvid@cypress.com>
2019-01-25 18:35:45 -0800

    PSOC6: add the latest Cypress PDL sources

---
+++ targets/TARGET_Cypress/TARGET_PSOC6/psoc6pdl/drivers/include/cy_crypto_core.h
@@ -0,0 +1,47 @@
+/***************************************************************************//**
+* \file cy_crypto_core.h
+* \version 2.20
+*
+* \brief
+*  This file provides common constants and parameters
+*  for the Crypto driver core interface.
+*
+********************************************************************************
+* Copyright 2016-2019 Cypress Semiconductor Corporation
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+
+#if !defined(CY_CRYPTO_CORE_H)
+#define CY_CRYPTO_CORE_H
+
+#include "cy_crypto_core_aes.h"
+#include "cy_crypto_core_crc.h"
+#include "cy_crypto_core_cmac.h"
+#include "cy_crypto_core_des.h"
+#include "cy_crypto_core_ecc.h"
+#include "cy_crypto_core_ecc_nist_p.h"
+#include "cy_crypto_core_hmac.h"
+#include "cy_crypto_core_hw.h"
+#include "cy_crypto_core_prng.h"
+#include "cy_crypto_core_mem.h"
+#include "cy_crypto_core_rsa.h"
+#include "cy_crypto_core_sha.h"
+#include "cy_crypto_core_trng.h"
+#include "cy_crypto_core_vu.h"
+
+#endif /* #if !defined(CY_CRYPTO_CORE_H) */
+
+/* [] END OF FILE */
