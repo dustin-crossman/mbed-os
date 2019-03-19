99188d8a01cd823ef0b4611b9d8200fbcc254882
Roman Okhrimenko <roman.okhrimenko@cypress.com>
2019-03-19 12:09:51 +0200

    Fixed typos in keygen.py (missed commits while merging)

--- targets/TARGET_Cypress/TARGET_PSOC6/provisioning-standalone/keygen.py
+++ targets/TARGET_Cypress/TARGET_PSOC6/provisioning-standalone/keygen.py
@@ -16,18 +16,15 @@ kid_help = 'Key ID to define key slot number in the key storage. Key ID must be 
               type=click.File('w'), 
               default='key.json',
               help='Name of the key in JWK format to create.')
-@click.option('--pem_priv', 'pemPriv',
+@click.option('--pem-priv', 'pemPriv',
               type=click.File('wb'), 
-              default='key_priv.pem',
-              help='Name of the private key in PEM format to create.')
-@click.option('--pem_pub', 'pemPub', 
+              default=None,
+              help='Name of the private key in PEM format to create. If it is not given PEM file will not be created.')
+@click.option('--pem-pub', 'pemPub',
               type=click.File('wb'), 
-              default='key_pub.pem',
-              help='Name of the public key in PEM format to create.')
-@click.option('--pem/--no-pem', 'pemEnable', 
-              default=False,
-              help='Enable creation of PEM files')
-def main(kId, jwKey, pemPriv, pemPub, pemEnable):
+              default=None,
+              help='Name of the public key in PEM format to create. If it is not given PEM file will not be created.')
+def main(kId, jwKey, pemPriv, pemPub):
     key = jwk.JWK.generate(kty='EC', crv='P-256', use='sig')
     
     keyJson = json.loads(key.export(private_key=True))
@@ -35,9 +32,11 @@ def main(kId, jwKey, pemPriv, pemPub, pemEnable):
     
     keyStr = json.dumps(keyJson, indent=4)
     jwKey.write(keyStr)
-    
-    if pemEnable:
+
+    if pemPriv != None:
         pemPriv.write(key.export_to_pem(private_key=True, password=None))
+
+    if pemPub != None:
         pemPub.write(key.export_to_pem(private_key=False, password=None))
     
     print(keyStr)
