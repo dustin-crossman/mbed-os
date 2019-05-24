3f67cfa7d472299847d78be071627d41458990b1
Volodymyr Medvid <volodymyr.medvid@cypress.com>
2019-05-24 13:37:38 +0300

    Update CI config

--- cy-build.sh
+++ cy-build.sh
@@ -13,33 +13,33 @@ _arm_compiler=""
 [[ $1 = win ]]     && _arm_compiler=Win64
 
 # install ARM_GNU
-#if [[ ${_arm_gnu} != "" ]]; then
-  #devops_scripts/install_tool.sh git@git-ore.aus.cypress.com:devops/tools/ARM_GNU.git gcc_631 ${_arm_gnu}
-  #export MBED_GCC_ARM_PATH=$(pwd)/ARM_GNU/${_arm_gnu}/bin
-  #echo 'ARM_GNU/*' >> .mbedignore
-#fi
+if [[ ${_arm_gnu} != "" ]]; then
+  devops_scripts/install_tool.sh git@git-ore.aus.cypress.com:devops/tools/ARM_GNU.git gcc_631 ${_arm_gnu}
+  export MBED_GCC_ARM_PATH=$(pwd)/ARM_GNU/${_arm_gnu}/bin
+  echo 'ARM_GNU/*' >> .mbedignore
+fi
 
 # install ARM_Compiler
-#if [[ ${_arm_compiler} != "" ]]; then
-#  devops_scripts/install_tool.sh git@git-ore.aus.cypress.com:devops/tools/ARM_Compiler.git arm_compiler_611 ${_arm_compiler}
-#  export MBED_ARMC6_PATH=$(pwd)/ARM_Compiler/${_arm_compiler}/bin
-#  export ARMLMD_LICENSE_FILE=8224@armlmd-aus-prod-wan.licenses.cypress.com
-#  echo 'ARM_Compiler/*' >> .mbedignore
-#fi
+if [[ ${_arm_compiler} != "" ]]; then
+  devops_scripts/install_tool.sh git@git-ore.aus.cypress.com:devops/tools/ARM_Compiler.git arm_compiler_611 ${_arm_compiler}
+  export MBED_ARMC6_PATH=$(pwd)/ARM_Compiler/${_arm_compiler}/bin
+  export ARMLMD_LICENSE_FILE=8224@armlmd-aus-prod-wan.licenses.cypress.com
+  echo 'ARM_Compiler/*' >> .mbedignore
+fi
 
 # install virtualenv
-#[[ $1 = mac ]]     && virtualenv env
-#[[ $1 = ubuntu ]]  && python3 -m virtualenv env
-#[[ $1 = win ]]     && virtualenv env
-#[[ $1 = mac ]]     && source env/bin/activate
-#[[ $1 = ubuntu ]]  && source env/bin/activate
-#[[ $1 = win ]]     && env/Scripts/activate.bat
-#echo 'env/*' >> .mbedignore
+[[ $1 = mac ]]     && virtualenv env
+[[ $1 = ubuntu ]]  && python3 -m virtualenv env
+[[ $1 = win ]]     && virtualenv env
+[[ $1 = mac ]]     && source env/bin/activate
+[[ $1 = ubuntu ]]  && source env/bin/activate
+[[ $1 = win ]]     && env/Scripts/activate.bat
+echo 'env/*' >> .mbedignore
 
 # install mbed-cli
-#pip install mbed-cli --no-cache-dir
-#pip install -r requirements.txt --no-cache-dir
-#mbed --version
+pip install mbed-cli --no-cache-dir
+pip install -r requirements.txt --no-cache-dir
+mbed --version
 wd=`pwd`
 
 windows=$(bash --version | grep msys | wc -l)
