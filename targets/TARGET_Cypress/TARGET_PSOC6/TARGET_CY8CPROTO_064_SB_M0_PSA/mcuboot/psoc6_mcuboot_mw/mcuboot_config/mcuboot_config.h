def5a01173f36fe881669dde61d335b904641751
Bohdan Kovalchuk <bohd@cypress.com>
2019-06-03 15:40:15 +0300

    fwsecurity-379: moving assert-DEFINE to targets.json to be obvious option

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB_M0_PSA/mcuboot/psoc6_mcuboot_mw/mcuboot_config/mcuboot_config.h
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB_M0_PSA/mcuboot/psoc6_mcuboot_mw/mcuboot_config/mcuboot_config.h
@@ -128,6 +128,6 @@
 /* Uncomment if your platform has its own mcuboot_config/mcuboot_assert.h.
  * If so, it must provide an ASSERT macro for use by bootutil. Otherwise,
  * "assert" is used. */
-#define MCUBOOT_HAVE_ASSERT_H
+/* #define MCUBOOT_HAVE_ASSERT_H */
 
 #endif /* __MCUBOOT_CONFIG_H__ */
